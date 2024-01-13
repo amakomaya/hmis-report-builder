b06dbb25195d386dc80a8a7ff201f45e61a5cf86
kharelabhishek <kharela86@gmail.com>
2024-01-13 17:16:28 +0545

    initial push

---
+++ d2.config.js
@@ -0,0 +1,11 @@
+const config = {
+    type: 'app',
+    name: "hmis-report-builder",
+    title: "HMIS-Report Builder",
+    description: "HMIS-Report Builder App",
+    entryPoints: {
+        app: './src/App.js',
+    },
+}
+
+module.exports = config
